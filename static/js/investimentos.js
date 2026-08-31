3f14fb9120b4e5cddcbf0a6ed0bc650bfaa37ce5
Gabriel N Vinha <gabrielnogueiravinha@gmail.com>
2026-08-31 16:39:57 -0300

    Add files via upload

--- static/js/investimentos.js
+++ static/js/investimentos.js
@@ -2,8 +2,11 @@ const requisicao = fetch("http://127.0.0.1:5000/investimentos/ativos");
 
 requisicao.then(resposta => resposta.json())
 .then(dados => {
-    console.log(dados);
-
+    if (dados.length == 0){} 
+    else {}
+position: fixed;
+    right: 20px;
+    bottom: 20px;
 })
 .catch(erro => {console.error("Erro na requisição:", erro);});
 
